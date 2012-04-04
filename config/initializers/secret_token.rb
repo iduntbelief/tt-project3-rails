fceb3f7f695f0de1993dda3a1e4a8d137c61b66b
Edward Ah Loy Chang <edward2@hawaii.edu>
2012-04-04 10:13:03 -1000

    Initial commit for TicketTaker Rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TtakerRails::Application.config.secret_token = '30a22ba753beecdf36435a49892f123ae652651797b6a386ba05062dd734a8bd2c78c62ca94ebc592281c5479a903e92dd90155a3d10b4ec9b74378ddd4efdb3'
