38feca831ca419652cdeb2f96de8a981f4f23b8c
Edward Ah Loy Chang <edward2@hawaii.edu>
2012-04-10 01:17:13 -1000

    Add login method to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,26 @@ class UsersController < ApplicationController
   end
 
   def login
+    user_id = -2
+    cookies.signed[:uid] = user_id
+    conn = ActiveRecord::Base.connection
+    if params[:username] == nil or params[:password] == nil
+      username = password = ""
+    else
+      username = params[:username]
+      password = params[:password]
+      user_id = conn.select_value("select getID('" + username +
+                                  "','" + password + "')").to_i
+    end
+
+    if user_id == -1
+      flash.now[:error] = 'Login failed.'
+
+    elsif user_id > 0
+      cookies.signed[:uid] = user_id
+      #redirect_to :controller => "users", :action => "index"
+      flash.now[:error] = cookies.signed[:uid]
+    end
   end
 
 end
